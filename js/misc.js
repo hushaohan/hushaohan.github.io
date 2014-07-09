2135902631d569942483d6706d0df1fc6cab459b
Shaohan <hushaohan@gmail.com>
2014-07-09 09:50:41 -0700

    added img fade in onload; adjusted shadow; adjusted img full screen background color

--- js/misc.js
+++ js/misc.js
@@ -1,12 +1,22 @@
-      $('[data-toggle="tooltip"]').tooltip({'placement': 'top'});
-      $(window).load( function() {$('img').smoothZoom({resizeDelay: 21,zoominSpeed: 521,zoomoutSpeed: 521,});});
-	  $("a[href^='#']").on('click', function(e) {
-		 target = this.hash;
-	     e.preventDefault();
-	     var hash = this.hash;
-	     $('html, body').animate({
-	         scrollTop: $(this.hash).offset().top
-	       }, 300, function(){
-			 window.location.hash = target;
-	       });
-	  });
+$('[data-toggle="tooltip"]').tooltip({
+	'placement': 'top'
+});
+
+$(window).load( function() {
+	$('img').smoothZoom({
+		resizeDelay: 21,
+		zoominSpeed: 611,
+		zoomoutSpeed: 611,
+	});
+});
+
+$("a[href^='#']").on('click', function(e) {
+	target = this.hash;
+	e.preventDefault();
+	var hash = this.hash;
+	$('html, body').animate({
+		scrollTop: $(this.hash).offset().top
+	}, 300, function(){
+		window.location.hash = target;
+	});
+});
