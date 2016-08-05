6270819fdea8f8b47708474869cf6939b8f1eb29
Shaohan Hu <hushaohan@gmail.com>
2016-08-05 10:33:28 -0500

    added return-to-top button

--- js/misc.js
+++ js/misc.js
@@ -26,3 +26,18 @@ $("img").one("load", function(evt) {
 }).each(function(evt) {
   	if(this.complete) $(this).load();
 });
+
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
